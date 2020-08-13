a0c9c704c5d29a1db2f500ce9e0edcd47013b19c
Aurel Hudec <hudec117@gmail.com>
2020-08-13 22:52:23 +0100

    Updated grid parser

--- src/robo.race/src/robo/race/GridParser.java
+++ src/robo.race/src/robo/race/GridParser.java
@@ -1,6 +1,7 @@
 package robo.race;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -9,50 +10,64 @@ import robo.race.entities.Gear;
 import robo.race.entities.GridEntity;
 import robo.race.entities.Pit;
 import robo.race.entities.RotationDirection;
+import robo.race.entities.StartingPosition;
 
 public class GridParser {
-	public Grid parse(String filePath) throws FileNotFoundException {
+	public Grid parse(String filePath) throws FileNotFoundException, GridParserException {
 		
 		File file = new File(filePath);
 		Scanner sc = new Scanner(file);
 		
-		ArrayList<ArrayList<GridEntity>> grid = new ArrayList<ArrayList<GridEntity>>();
-
-		int lineCount = 0;
-		while (sc.hasNextLine()) {
-			if (lineCount > 0) {
-				String line = sc.nextLine();
-				char[] rawEntities = line.toCharArray();
-				ArrayList<GridEntity> entities = new ArrayList<GridEntity>();
-				
-				for (char rawEntity : rawEntities) {
-					GridEntity entityToAdd = null;
+		ArrayList<GridEntity[]> allEntities = new ArrayList<GridEntity[]>();
+		
+		try {
+			int lineCount = 0;
+			while (sc.hasNextLine()) {
+				if (lineCount > 0) {
+					String line = sc.nextLine();
+					char[] rawEntities = line.toCharArray();
 					
-					if (rawEntity == 'x') {
-						// Pit
-						entityToAdd = new Pit();
-					} else if (rawEntity == '+' || rawEntity == '-') {
-						// Gear
-						RotationDirection direction = rawEntity == '+' ? RotationDirection.Clockwise : RotationDirection.AntiClockwise;
-						entityToAdd = new Gear(direction);
-					} else if (Character.isDigit(rawEntity)) {
-						// Flag
-						int flagNumber = rawEntity - '0';
-						if (flagNumber < 1 || flagNumber > 4) {
-							// TODO: handle
+					GridEntity[] entities = new GridEntity[rawEntities.length];
+					
+					for (int i = 0; i < rawEntities.length;) {
+						char rawEntity = rawEntities[i];
+						GridEntity entityToAdd = null;
+						
+						if (rawEntity == 'x') {
+							// Pit
+							entityToAdd = new Pit();
+						} else if (rawEntity == '+' || rawEntity == '-') {
+							// Gear
+							RotationDirection direction = rawEntity == '+' ? RotationDirection.Clockwise : RotationDirection.AntiClockwise;
+							entityToAdd = new Gear(direction);
+						} else if (Character.isDigit(rawEntity)) {
+							// Flag
+							int flagNumber = rawEntity - '0';
+							if (flagNumber < 1 || flagNumber > 4) {
+								throw new GridParserException("Board contains more than 4 flags.");
+							}
+							entityToAdd = new Flag(flagNumber);
+						} else if (Character.isUpperCase(rawEntity)) {
+							// Robot
+							if (rawEntity > 'D') {
+								throw new GridParserException("Board cannot have more than 4 players.");
+							}
+							entityToAdd = new StartingPosition(rawEntity);
 						}
-						entityToAdd = new Flag(flagNumber);
-					} else if (Character.isUpperCase(rawEntity)) {
-						// Robot
-//						entityToAdd = new Robot();
+						
+						entities[i] = entityToAdd;
 					}
 					
-					entities.add(entityToAdd);
+					allEntities.add(entities);
 				}
 				
-				grid.add(entities);
+				lineCount++;
 			}
+		} finally {
+			sc.close();
 		}
+		
+		Grid grid = new Grid((GridEntity[][])allEntities.toArray());
 		
 		return grid;
 	}
---
+++ src/robo.race/src/robo/race/GridParserException.java
@@ -0,0 +1,10 @@
+package robo.race;
+
+public class GridParserException extends Exception {
+
+	private static final long serialVersionUID = 2054416495460113634L;
+
+	public GridParserException(String message) {
+		super(message);
+	}
+}
