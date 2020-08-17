54c1769d738d286b5eaaa107de18eb76f78676f7
Muhammad <talha.muhammad@capgemini.com>
2020-08-17 20:08:04 +0100

    Added Override + ToString method

--- src/robo.race/src/robo/race/entities/Pit.java
+++ src/robo.race/src/robo/race/entities/Pit.java
@@ -1,11 +1,19 @@
 package robo.race.entities;
 
 public class Pit extends GridEntity {
-	void act() {
+	@Override
+	public void act(Robot robot) {
 		 throw new UnsupportedOperationException("Invalid operation");
 	}
 	
-	void react() {
-		Robot.destroy();
+	@Override
+	public void react(Robot robot) {
+		robot.destroy();
 	}
+	
+	@Override
+	public String toString() {
+		return "X";
+	}	
+	
 }
